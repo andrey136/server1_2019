bc98e1eef6cd2a1e165544cd305b280ad8888c03
andrey_mardash <andrey.mardash@gmail.com>
2019-09-01 07:41:36 +0300

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const uniqid = require('uniqid');
+let users = [{
+    login: "andrey.mardash@gmail.com",
+    password: "2002126server",
+    id: "5CFEE39D812461A67D865C819934895A3FB1A23934941847217A5B5ECB862FDBB2D41B538F",
+    status: "admin",
+  }
+];
+
 let list = [{
   title: 'Some',
   id: uniqid(),
@@ -32,12 +40,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+app.get("/5CFEE39D812461A67D865C819934895A3FB1A23934941847217A5B5ECB862FDBB2D41B538F",(req, res) =>{
+    console.log(req, res);
+    res.status(201).json({message: true});
+  }
+);
+
 app.get('/',(req, res) =>{
     console.log(req, res);
     res.status(201).json(list);
   }
 );
 
+app.post('/authorization',(req, res) => {
+  setTimeout(() => {
+    console.log(req, res);
+    if(req.body.login === users[0].login && req.body.password === users[0].password){
+      res.status(200).json({ message: 'Well done:)', id: users[0].id, status: "admin"});
+    } else {
+      res.status(401).json({message: "Authorization failed."})
+    }
+  }, 2000);
+});
 
 app.post('/',(req, res) => {
   setTimeout(() => {
