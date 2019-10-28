21920fa56f73b594592bcbb7949ee56bd562fcbc
andrey_mardash <andrey.mardash@gmail.com>
2019-10-28 19:30:57 +0300

    updates

--- routes/api.js
+++ routes/api.js
@@ -46,7 +46,15 @@ router.delete('/list/deleteItem/:id', (req, res) => {
   NewUser.findById(req.params.id).then((user) => {
     const list = [];
     user.list.map(el => el.id === req.body.id && list.push(...user.list.filter(item => item !== el)));
+    console.log(list);
     NewUser.findByIdAndUpdate({_id: req.params.id},{list: list}).then(() => res.send(list));
+  }).catch(err => res.send({message: "ERROR: something went wrong"}));
+});
+
+router.put('/list/changeDone/:id', (req, res) => {
+  NewUser.findById(req.params.id).then((user) => {
+    user.list.map(el => el.id === req.body.id ? el.done = !el.done : '');
+    NewUser.findByIdAndUpdate({_id: req.params.id},{list: user.list}).then(() => res.send(user.list));
   }).catch(err => res.send({message: "ERROR: something went wrong"}));
 });
 
